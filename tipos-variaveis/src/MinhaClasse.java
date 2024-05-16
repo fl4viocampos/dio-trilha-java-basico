76a2a6dfe7be7c97b160bb559eccde71dd8ecbf5
fl4viocampos <flavioscampos1980@gmail.com>
2024-05-16 18:16:00 -0300

    Meu projeto Conta Banco

---
+++ tipos-variaveis/src/MinhaClasse.java
@@ -0,0 +1,7 @@
+public class MinhaClasse {
+    public static void main(String[] args){
+        System.out.println("oi, turma fui executado pelo Terminal");
+    }
+    
+}
+public String formatarCep(long cep){}
--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,9 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto = (short) numeroNormal
     }
 }

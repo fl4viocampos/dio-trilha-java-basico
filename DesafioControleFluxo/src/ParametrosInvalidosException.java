f40851275ecd0c17e124b6836cf6ace0a30565cf
fl4viocampos <flavioscampos1980@gmail.com>
2024-05-22 17:16:31 -0300

    Desafio Pronto finalizado

--- DesafioControleFluxo/src/ParametrosInvalidosException.java
+++ DesafioControleFluxo/src/ParametrosInvalidosException.java
@@ -1,5 +1,5 @@
 public class ParametrosInvalidosException extends Exception {
     public ParametrosInvalidosException(String mensagem) {
-        super("O segundo parametro deve ser maior que o primeiro");
+        super("O segundo parametro deve ser maior que o primeiro ");
     }
 }
